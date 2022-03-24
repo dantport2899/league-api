d874d2dcb1b6f33d358264746892b1014b36bda8
Dante Portales <dantport28@gmail.com>
2022-03-24 13:23:03 -0600

    maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './components/start/start.component';
+import { SummonerComponent } from './components/summoner/summoner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    SummonerComponent
   ],
   imports: [
     BrowserModule
